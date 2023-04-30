74f47dfe26da212089f73c4d56eb3816da8876e1
anasjy <anas.ansari18@gmail.com>
2023-05-01 00:17:13 +0530

    frontend complete on anmazon clone by anas

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCtUD3IrqxTAKnksDSq-stkjige_UC2s-I",
+  authDomain: "challenge-6d1a8.firebaseapp.com",
+  projectId: "challenge-6d1a8",
+  storageBucket: "challenge-6d1a8.appspot.com",
+  messagingSenderId: "806048259260",
+  appId: "1:806048259260:web:5e1f7b1cd5582c96a3c4b9",
+  measurementId: "G-47C5GT9F9K"
+};
