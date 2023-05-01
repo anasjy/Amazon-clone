feee21988878f8bdc63b64ef522c2f446afec9d8
anasjy <anas.ansari18@gmail.com>
2023-05-02 00:47:23 +0530

    recent update after creating sign in page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Checkout from './Checkout';
 import Login from './Login';
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+// import { loadStripe } from "@stripe/stripe-js";
+// import { Elements } from "@stripe/react-stripe-js";
 
 function App() {
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,14 @@
+
+// import firebase from "firebase";
+
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCtUD3IrqxTAKnksDSq-stkjige_UC2s-I",
-  authDomain: "challenge-6d1a8.firebaseapp.com",
-  projectId: "challenge-6d1a8",
-  storageBucket: "challenge-6d1a8.appspot.com",
-  messagingSenderId: "806048259260",
-  appId: "1:806048259260:web:5e1f7b1cd5582c96a3c4b9",
-  measurementId: "G-47C5GT9F9K"
-};
+
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+
+// export { db, auth };
